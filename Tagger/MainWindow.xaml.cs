ea92b9de65238fef737e7265f6fb625b43094aa9
U-MaorLaptop\Maor <Maor@MaorLaptop.(none)>
2017-06-02 21:14:33 +0300

    lost

--- Tagger/MainWindow.xaml.cs
+++ Tagger/MainWindow.xaml.cs
@@ -5,14 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using TaggingBackend;
 
 namespace Tagger
 {
@@ -23,29 +26,49 @@ namespace Tagger
     {
         PlayStateController playStateController;
         DispatcherTimer timer;
+        bool userIsDraggingSlider = false;
+        bool suppressMediaPositionUpdate = false;
+        TagSaver tagSaver;
+
         public MainWindow()
         {
             InitializeComponent();
             playStateController = new PlayStateController();
-            playStateController.DisableCommand += (s, e) => { btnPlay.IsEnabled = false; timelineSlider.IsEnabled = false;};
-            playStateController.EnableCommand += (s, e) => { btnPlay.IsEnabled = true; timelineSlider.IsEnabled = true; timelineSlider.Value = 0; timer.Start(); };
+            playStateController.DisableCommand += (s, e) => { btnPlay.IsEnabled = false; timelineSlider.IsEnabled = false; /*tagBox.IsEnabled = false;*/ };
+            playStateController.EnableCommand += (s, e) => { btnPlay.IsEnabled = true; timelineSlider.IsEnabled = true; timelineSlider.Value = 0; timer.Start();/* tagBox.IsEnabled = true;*/ };
             playStateController.PauseCommand += (s, e) => { btnPlay.Content = FindResource("Play"); mePlayer.Pause(); };
-            playStateController.PlayCommand += (s, e) => { btnPlay.Content = FindResource("Pause"); mePlayer.Play(); };
+            playStateController.PlayCommand += (s, e) => { btnPlay.Content = FindResource("Pause"); mePlayer.Play(); RenewSliderPosition(); };
 
 
             timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromMilliseconds(200);
-            timer.Tick += new EventHandler(sliderTick);     
-             
+            timer.Tick += new EventHandler(sliderTick);
+
+            //tagBox.Background = new SolidColorBrush();
+
+            tagSaver = new TagSaver();
         }
 
         private void sliderTick(object sender, EventArgs e)
         {
-            if (mePlayer.NaturalDuration.HasTimeSpan)
+            RenewSliderPosition();
+        }
+
+        private void RenewSliderPosition()
+        {
+            if (mePlayer.NaturalDuration.HasTimeSpan && !userIsDraggingSlider)
             {
-                timelineSlider.Value = mePlayer.Position.TotalSeconds /
-                                           mePlayer.NaturalDuration.TimeSpan.TotalSeconds;
+                suppressMediaPositionUpdate = true;
+                timelineSlider.Value = CalcTimelineRelativePosition();
             }
+
+        }
+
+        private double CalcTimelineRelativePosition()
+        {
+            return mePlayer.Position.TotalSeconds /
+                                                   mePlayer.NaturalDuration.TimeSpan.TotalSeconds;
+;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -60,5 +83,51 @@ namespace Tagger
 
             playStateController.Enable();
         }
+
+        private void sliProgress_DragStarted(object sender, DragStartedEventArgs e)
+        {
+            userIsDraggingSlider = true;
+        }
+
+        private void sliProgress_DragCompleted(object sender, DragCompletedEventArgs e)
+        {
+            userIsDraggingSlider = false;
+            suppressMediaPositionUpdate = true;
+            mePlayer.Position = TimeSpan.FromSeconds(timelineSlider.Value * mePlayer.NaturalDuration.TimeSpan.TotalSeconds);
+        }
+
+        private void SeekToMediaPosition(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            // Making sure the position doesn't keep updating when the timer ticks or when a user is dragging the video around
+            if (userIsDraggingSlider) return;
+            if (suppressMediaPositionUpdate)
+            {
+                suppressMediaPositionUpdate = false;
+                return;
+            }
+
+            if (mePlayer.NaturalDuration.HasTimeSpan){
+                mePlayer.Position = TimeSpan.FromSeconds(timelineSlider.Value * mePlayer.NaturalDuration.TimeSpan.TotalSeconds);
+            }
+        }
+
+        private void keyUpTagBox(object sender, KeyEventArgs a)
+        {
+            if (a.Key == Key.Enter)
+            {
+                string tag = tagBox.Text;
+                tagBox.Text = string.Empty;
+
+                ColorAnimation animation;
+                animation = new ColorAnimation();
+                animation.To = Colors.LightGreen;
+                animation.Duration = new Duration(TimeSpan.FromMilliseconds(350));
+                animation.AutoReverse = true;
+                tagBox.Background.BeginAnimation(SolidColorBrush.ColorProperty, animation);
+
+                tagSaver.Save(new Tag() { TagContent = tag, SecondsSinceStart = (int)mePlayer.Position.TotalSeconds, VideoName = mePlayer.Source.ToString() });
+            }
+        }
+        
     }
 }
